f87e8f4e2f5e57073f1f1cef47110b4056ddfb6f
zsh2401 <zsh2401@163.com>
2019-11-25 20:06:37 +0800

    bug fixes

--- src/view/pages/ReadPage/index.tsx
+++ src/view/pages/ReadPage/index.tsx
@@ -2,25 +2,24 @@ import React from 'react'
 import { Switch, Route } from 'react-router'
 import AppTemplate from '../../components/AppTemplate'
 import NavBar from '../../components/NavBar'
-import { LodableComponent } from '../../components'
 import Letters from './Letters'
 import Tea from './Tea'
 import useBackToTop from '../../../common/hooks/useBackToTop'
 export default function ReadPage({ match }: any) {
     useBackToTop();
     return <AppTemplate container={false} nonavbar>
-        <NavBar className="sticky-top"></NavBar>
+        <NavBar className="sticky-top" />
         <Switch>
             <Route path={`${match.url}/loveletters`} component={GLetters} />
             <Route path={`${match.url}/tea`} component={GTea} />
         </Switch>
     </AppTemplate>
 }
-function GLetters(){
+function GLetters() {
     // return <LodableComponent loader={()=>import(/*webpackChunkName:"letters" */ "./Letters")}/>
-    return <Letters/>;
+    return <Letters />;
 }
-function GTea(){
+function GTea() {
     // return <LodableComponent loader={()=>import(/*webpackChunkName:"tea" */"./Tea")}/>
-    return <Tea/>;
+    return <Tea />;
 }
